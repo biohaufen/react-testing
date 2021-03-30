0cbc3f5eae4e395ff15d8ca0e21d3d119c4ca7c7
David Herbst Ayala <mail@david-it.dev>
2021-03-30 11:53:36 +0200

    implemented shallow test

---
+++ src/components/UserStory.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import UserStory from './UserStory'
+
+describe('Test UserStory finish component', () => {
+    it('Test click event', () => {
+      const mockCallBack = jest.fn();
+  
+      const userStory = shallow((<UserStory updateStatus={mockCallBack} />));
+      userStory.find('[data-behavior="finish"]').simulate('click');
+      expect(mockCallBack.mock.calls).toEqual([["finished"]]);
+    });
+});
+
+describe('Test UserStory deliver component', () => {
+    it('Test click event', () => {
+      const mockCallBack = jest.fn();
+  
+      const userStory = shallow((<UserStory updateStatus={mockCallBack} />));
+      userStory.find('[data-behavior="deliver"]').simulate('click');
+      expect(mockCallBack.mock.calls).toEqual([["delivered"]]);
+    });
+});
+
